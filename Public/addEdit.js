e3d8cdb557bde1a982d38da6f8e560a5d8d9826d
Davlee1 <davidlee21019@gmail.com>
2026-02-22 01:41:52 -0500

    combed addEdit for incorrect references

--- Public/addEdit.js
+++ Public/addEdit.js
@@ -8,155 +8,159 @@ let status = null;
 let addingItem = null;
 
 export const handleAddEdit = () => {
-    addEditDiv = document.getElementById("edit-item");
-    imageURL = document.getElementById("imageURL");
-    link = document.getElementById("link");
-    description = document.getElementById("description");
-    priority = document.getElementById("priority");
-    addingItem = document.getElementById("adding-item");
-    const editCancel = document.getElementById("edit-cancel");
+  addEditDiv = document.getElementById("edit-item");
+  imageURL = document.getElementById("imageURL");
+  link = document.getElementById("link");
+  description = document.getElementById("description");
+  priority = document.getElementById("priority");
+  addingItem = document.getElementById("adding-item");
+  const editCancel = document.getElementById("edit-cancel");
 
-    addEditDiv.addEventListener("click", async (e) => {
-        if (inputEnabled && e.target.nodeName === "BUTTON") {
-            if (e.target === addingJob) {
-                enableInput(false);
-
-                let method = "POST";
-                let url = "/api/v1/items";
-                try {
-                    const response = await fetch(url, {
-                        method: method,
-                        headers: {
-                            "Content-Type": "application/json",
-                            Authorization: `Bearer ${token}`,
-                        },
-                        body: JSON.stringify({
-                            imageURL: imageURL.value,
-                            link: link.value,
-                            description: description.value,
-                            priority: priority.value,
-                        }),
-                    });
-
-                    const data = await response.json();
-                    if (response.status === 201) {
-                        // 201 indicates a successful create
-                        message.textContent = "The item entry was created.";
-
-                        company.value = "";
-                        position.value = "";
-                        status.value = "pending";
-
-                        showJobs();
-                    } else {
-                        message.textContent = data.msg;
-                    }
-                } catch (err) {
-                    console.log(err);
-                    message.textContent = "A communication error occurred.";
-                }
-
-                enableInput(true);
-            } else if (e.target === editCancel) {
-                message.textContent = "";
-                showItems();
-            }
-        }
-
-        if (e.target === addingJob) {
-  enableInput(false);
-
-  let method = "POST";
-  let url = "/api/v1/wishlist";
-
-  if (addingJob.textContent === "update") {
-    method = "PATCH";
-    url = `/api/v1/wishlist/${addEditDiv.dataset.id}`;
-  }
-
-  try {
-    const response = await fetch(url, {
-      method: method,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        company: company.value,
-        position: position.value,
-        status: status.value,
-      }),
-    });
-
-    const data = await response.json();
-    if (response.status === 200 || response.status === 201) {
-      if (response.status === 200) {
-        // a 200 is expected for a successful update
-        message.textContent = "The item entry was updated.";
-      } else {
-        // a 201 is expected for a successful create
-        message.textContent = "The item entry was created.";
-      }
-
-      company.value = "";
-      position.value = "";
-      status.value = "pending";
-      showItems();
-    } else {
-      message.textContent = data.msg;
-    }
-  } catch (err) {
-    console.log(err);
-    message.textContent = "A communication error occurred.";
-  }
-  enableInput(true);
-}
-    });
-    
-};
-
-export const showAddEdit = async (itemId) => {
-    if (!itemId) {
-        company.value = "";
-        position.value = "";
-        status.value = "pending";
-        addingJob.textContent = "add";
-        message.textContent = "";
-
-        setDiv(addEditDiv);
-    } else {
+  addEditDiv.addEventListener("click", async (e) => {
+    if (inputEnabled && e.target.nodeName === "BUTTON") {
+      if (e.target === addingItem) {
         enableInput(false);
 
+        let method = "POST";
+        let url = "/api/v1/wishlist";
         try {
-            const response = await fetch(`/api/v1/wishlist/${itemId}`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                },
-            });
+          const response = await fetch(url, {
+            method: method,
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+              imageURL: imageURL.value,
+              link: link.value,
+              description: description.value,
+              priority: priority.value,
+            }),
+          });
 
-            const data = await response.json();
-            if (response.status === 200) {
-                company.value = data.item.company;
-                position.value = data.item.position;
-                status.value = data.item.status;
-                addingJob.textContent = "update";
-                message.textContent = "";
-                addEditDiv.dataset.id = itemId;
+          const data = await response.json();
+          if (response.status === 201) {
+            // 201 indicates a successful create
+            message.textContent = "The item entry was created.";
 
-                setDiv(addEditDiv);
-            } else {
-                // might happen if the list has been updated since last display
-                message.textContent = "The item entry was not found";
-                showItems();
-            }
-        } catch (err) {
-            console.log(err);
-            message.textContent = "A communications error has occurred.";
+            imageURL.value = "";
+            link.value = "";
+            description.value = "";
+            priority.value = "";
+
             showItems();
+          } else {
+            message.textContent = data.msg;
+          }
+        } catch (err) {
+          console.log(err);
+          message.textContent = "A communication error occurred.";
         }
 
         enableInput(true);
+      } else if (e.target === editCancel) {
+        message.textContent = "";
+        showItems();
+      }
     }
+
+    if (e.target === addingJob) {
+      enableInput(false);
+
+      let method = "POST";
+      let url = "/api/v1/wishlist";
+
+      if (addingItem.textContent === "update") {
+        method = "PATCH";
+        url = `/api/v1/wishlist/${addEditDiv.dataset.id}`;
+      }
+
+      try {
+        const response = await fetch(url, {
+          method: method,
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({
+            imageURL: imageURL.value,
+            link: link.value,
+            description: description.value,
+            priority: priority.value,
+          }),
+        });
+
+        const data = await response.json();
+        if (response.status === 200 || response.status === 201) {
+          if (response.status === 200) {
+            // a 200 is expected for a successful update
+            message.textContent = "The item entry was updated.";
+          } else {
+            // a 201 is expected for a successful create
+            message.textContent = "The item entry was created.";
+          }
+
+          imageURL.value = "";
+          link.value = "";
+          description.value = "";
+          priority.value = "";
+          showItems();
+        } else {
+          message.textContent = data.msg;
+        }
+      } catch (err) {
+        console.log(err);
+        message.textContent = "A communication error occurred.";
+      }
+      enableInput(true);
+    }
+  });
+};
+
+export const showAddEdit = async (itemId) => {
+  if (!itemId) {
+    imageURL.value = "";
+    link.value = "";
+    description.value = "";
+    priority.value = "";
+    addingItem.textContent = "add";
+    message.textContent = "";
+
+    setDiv(addEditDiv);
+  } else {
+    enableInput(false);
+
+    try {
+      const response = await fetch(`/api/v1/wishlist/${itemId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await response.json();
+      if (response.status === 200) {
+        imageURL.value = data.item.imageURL;
+        link.value = data.item.link;
+        description.value = data.item.description;
+        priority.value = data.item.priority;
+        addingItem.textContent = "update";
+        message.textContent = "";
+        addEditDiv.dataset.id = itemId;
+
+        setDiv(addEditDiv);
+      } else {
+        // might happen if the list has been updated since last display
+        message.textContent = "The item entry was not found";
+        showItems();
+      }
+    } catch (err) {
+      console.log(err);
+      message.textContent = "A communications error has occurred.";
+      showItems();
+    }
+
+    enableInput(true);
+  }
 };
